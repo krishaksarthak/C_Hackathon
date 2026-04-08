a7bd345f811eea937e2e03bb0b333daa30ee4de5
skrishak <skrishak@visteon.com>
2026-04-08 09:53:12 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * File: main.c
+ * Author: Sarthak Krishak
+ * Date: 08/04/2026
+ * Description: Entry code for the 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Hello world\n");
+    return 0;
+}
